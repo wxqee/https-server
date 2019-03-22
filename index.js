4f4d38fe86fbc776286ba10f45c9d5fd1f1a9f58
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2019-03-22 12:39:39 +0800

    use public/

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var createServer = require('./create-server');
 var app = express();
+app.use(express.static('public'))
 var { httpServer, httpsServer } = createServer(app, { http: { port: process.env.PORT || 3000 } });
